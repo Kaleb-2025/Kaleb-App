7c84ca3ba8a5d5a36a3c8a443b8f6279d4c6755d
Kaleb-2025 <KALEB2025TCC@gmail.com>
2025-05-17 13:00:03 -0300

    Update CadastroInicial.js

--- Código/src/screens/Login/CadastroInicial.js
+++ Código/src/screens/Login/CadastroInicial.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import Logo from '../../components/Login/Logo';
 import SocialLoginButton from '../../components/Login/SocialLoginButton';
 import { CadastroStyles as styles } from '../../styles/CadastroStyle';
 import EmailSignupForm from '../../components/Login/Email';
 import { handleTermsPress, handlePrivacyPress } from '../../links/links.js';
 
-const CadastroInicial = () => {
+const CadastroInicial = ({ navigation }) => {
+  const handleEmailSignup = () => {
+    navigation.navigate('EmailPag');
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Logo />
@@ -24,23 +28,21 @@ const CadastroInicial = () => {
         />
       </View>
       <View style={styles.divider} />
-      <EmailSignupForm />
-         <View style={styles.footer}>
-          <Text style={styles.footerText}>
-            Ao se inscrever no Kaleb você concorda com os nossos{' '}
-            <Text onPress={handleTermsPress} style={styles.link}>
-              Termos de Uso
-            </Text>{' '}
-            e{' '}
-            <Text onPress={handlePrivacyPress} style={styles.link}>
-              Política de Privacidade.
-            </Text>
+    <EmailSignupForm onPress={handleEmailSignup} navigation={navigation} />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Ao se inscrever no Kaleb você concorda com os nossos{' '}
+          <Text onPress={handleTermsPress} style={styles.link}>
+            Termos de Uso
+          </Text>{' '}
+          e{' '}
+          <Text onPress={handlePrivacyPress} style={styles.link}>
+            Política de Privacidade.
           </Text>
-        </View>
+        </Text>
+      </View>
     </ScrollView>
-    
   );
 };
-
 
 export default CadastroInicial;
