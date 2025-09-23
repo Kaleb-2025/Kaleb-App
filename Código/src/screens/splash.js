3995c00714d09f286fce2f20c65491955b673ca3
Beatriz <132356935+abeatrizoliveira@users.noreply.github.com>
2025-09-22 22:29:22 -0300

    Update splash.js

--- Código/src/screens/splash.js
+++ Código/src/screens/splash.js
@@ -1,22 +1,39 @@
-import React, { useRef } from "react";
-import { StyleSheet, View } from "react-native";
-import { Video } from "expo-av";
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { useEvent } from 'expo';
+import React, {useEffect} from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function App() {
-  const videoRef = useRef(null);
+const videoSource =
+  'https://rsggftidydvuzvmealpg.supabase.co/storage/v1/object/public/splash/intro.mp4';
 
+export default function VideoScreen() {
+  const navigation = useNavigation();
+  const player = useVideoPlayer('https://rsggftidydvuzvmealpg.supabase.co/storage/v1/object/public/splash/intro.mp4', (player) => {
+    player.play();
+    player.loop = false;
+    player.timeUpdateEventInterval = 1000;
+  });
+
+  useEffect(() => {
+    const handlePlayToEnd = () => {
+      navigation.navigate('CadastroInicial');
+    };
+
+    const subscription = player.addListener('playToEnd', handlePlayToEnd);
+
+    return () => {
+      subscription.remove();
+    };
+  }, [player]);
+  
   return (
     <View style={styles.container}>
-      <Video
-        ref={videoRef}
-        source={{
-          uri: "https://video-link-generator.replit.app/api/videos/file/1758377308225-224467920.mp4",
-        }}
+      <VideoView
         style={StyleSheet.absoluteFill}
-        resizeMode="cover"  
-        shouldPlay           
-        isLooping={false}    
-        isMuted={true}       
+        player={player}
+        allowsFullscreen
+        allowsPictureInPicture
       />
     </View>
   );
@@ -25,5 +42,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#0b1658',
   },
 });
